f5ffa64183532d646cd4a32f5d619e88392c8367
ManojLL <manojlakshan421@gmail.com>
2025-02-15 15:26:28 +0530

    added unit test for Homepage

--- todo-frontend/src/tests/HomePage.test.js
+++ todo-frontend/src/tests/HomePage.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import HomePage from "../pages/HomePage";
+import { fetchTasks, saveTask, completeTask } from '../services/TaskApiService';
 
+jest.mock('../services/TaskApiService');
 
-test('render home page corectly', () => {
+test('renders TaskForm and TaskItems components', async () => {
+    render(<HomePage />);
 
+    expect(screen.getByRole('heading', { name: /Add a To-Do Task/i })).toBeInTheDocument();
+
+    expect(screen.getByPlaceholderText('Title')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Description')).toBeInTheDocument();
 });
+
